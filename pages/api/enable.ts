b54bc611eaebd3a5713add10f28eefd10079d697
Steve <heystevegray.dev@gmail.com>
2022-10-07 23:07:38 -0400

    Added status and enable buttons

--- pages/api/enable.ts
+++ pages/api/enable.ts
@@ -1,5 +1,35 @@
 import { NextApiResponse, NextApiRequest } from 'next';
+import fs from 'fs';
+import path from 'path';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ enabled: true });
+const filePath = 'data.json';
+const fullFilePath = path.join(__dirname, filePath);
+
+type ResponseData = {
+    message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+    try {
+        if (req.method === 'POST') {
+            const body = req.body;
+            console.log({ body });
+            try {
+                fs.promises.writeFile(fullFilePath, JSON.stringify(body));
+                console.log(`Successfully wrote to ${fullFilePath}`);
+                res.status(200).send({ message: 'success' });
+            } catch (error) {
+                console.log(error);
+                res.status(500).send({ message: `Failed to write to ${fullFilePath}` });
+            }
+        }
+
+        if (req.method === 'GET') {
+            const message = await fs.promises.readFile(fullFilePath, 'utf8');
+            res.status(200).send({ message });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: 'Something went wrong' });
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,51 @@
+import { Button, Typography } from '@mui/material';
 import Layout from 'components/Layout';
-import Summary from 'components/Summary';
 import { APP_DESCRIPTION, APP_NAME } from 'lib/config';
 import Head from 'next/head';
+import { useState } from 'react';
+import { ResponseData } from 'types/api';
 
 export default function Home() {
+    const [enabled, setEnabled] = useState(false);
+    const [response, setResponse] = useState('');
+
+    const handleStatus = async () => {
+        try {
+            await fetch('/api/enable', {
+                method: 'GET',
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    setResponse(response?.message);
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const toggleStart = async () => {
+        const nextState = !enabled;
+        setEnabled(nextState);
+
+        try {
+            await fetch('/api/enable', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    enable: nextState,
+                }),
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    setResponse(response?.message);
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
             <Head>
@@ -11,8 +53,14 @@ export default function Home() {
                 <meta name="description" content={APP_DESCRIPTION} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Layout icon="🤑" title="Summary" description="Your spending summary">
-                <Summary />
+            <Layout>
+                <Button variant="contained" color="primary" onClick={toggleStart}>
+                    {enabled ? 'Disable' : 'Enable'}
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleStatus}>
+                    Status
+                </Button>
+                <Typography>{response}</Typography>
             </Layout>
         </>
     );
