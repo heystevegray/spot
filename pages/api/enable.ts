884c822755587b1666249510b22f59eef0432c3f
Steve <heystevegray.dev@gmail.com>
2022-10-12 21:19:41 -0400

    Changed sound libs, it's working!

--- pages/api/enable.ts
+++ pages/api/enable.ts
@@ -1,8 +1,9 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 import fs from 'fs';
 import path from 'path';
-import sound from 'sound-play';
 import { Gpio } from 'onoff'; // include onoff to interact with the GPIO
+
+const player = require('play-sound')()
 
 const filePath = 'data.json';
 const fullFilePath = path.join('public', filePath);
@@ -15,9 +16,11 @@ const SENSOR: Gpio | undefined = isLinux ? new Gpio(motionPin, 'in') : undefined
 let monitor: NodeJS.Timer | undefined = undefined;
 
 let isPlaying = false;
+let audio: { kill: () => void; } | undefined = undefined
 
 type ResponseData = {
     message: string;
+    success: boolean
 };
 
 type Config = {
@@ -26,17 +29,30 @@ type Config = {
 
 const playSound = () => {
     const soundFile = path.join('public', 'sounds', 'Mark_Hamill_Joker.wav');
-    sound.play(soundFile).then(() => {
-        // Audio file is done
-        isPlaying = false;
-    });
+
+    try {
+        audio = player.play(soundFile, function (err: { killed: any; }) {
+            if (err && !err.killed) {
+                console.log("Audio was killed");
+            }
+
+            // Reset the playing status
+            isPlaying = false
+        })
+    } catch (error) {
+        console.log(error)
+        // Reset the playing status
+        isPlaying = false
+    }
+
 };
 
 const detectMotion = () => {
-    if (isLinux && SENSOR) {
-        if (!isPlaying) {
-            // Check the pin state 1 = on
-            try {
+    try {
+        if (isLinux && SENSOR) {
+            if (!isPlaying) {
+                // Check the pin state 1 = on
+
                 if (SENSOR.readSync() === 1) {
                     console.log('Motion Detected!!!!');
                     isPlaying = true;
@@ -44,64 +60,79 @@ const detectMotion = () => {
                 } else {
                     console.log('Watching...');
                 }
-            } catch (error) {
-                console.log('detectMotion', { error });
+
+            } else {
+                console.log('Sound is playing...');
             }
         } else {
-            console.log('Sound is playing...');
+            console.log(`Current platform is "${process.platform}". GPIO will only work on linux.`);
         }
-    } else {
-        console.log(`Current platform is "${process.platform}". GPIO will only work on linux.`);
-    }
-};
-
-const startSpotting = () => {
-    try {
-        console.log(`GPIO accessible: ${Gpio?.accessible}`);
     } catch (error) {
-        console.log({ error });
+        console.log('detectMotion', { error });
     }
-    monitor = setInterval(detectMotion, pollInterval);
-    console.log(`Started interval ${monitor}`);
 };
 
-const stopSpotting = () => {
+const startSpotting = (): ResponseData => {
+    if (Gpio?.accessible) {
+        console.log(`GPIO accessible: ${Gpio?.accessible}`);
+        monitor = setInterval(detectMotion, pollInterval);
+        console.log(`Started interval ${monitor}`);
+        return { message: `Started interval ${monitor}`, success: true }
+    }
+
+    console.log("Damn. GPIO is not accessible.");
+    return { message: `Damn. GPIO is not accessible. SENSOR: ${JSON.stringify(SENSOR)}`, success: false }
+
+
+};
+
+const stopSpotting = (): ResponseData => {
     if (monitor) {
         clearInterval(monitor);
         console.log(`Stopped interval ${monitor}`);
+
+        // Kill audio
+        audio?.kill()
+
+        return {
+            message: `Stopped interval ${monitor}`, success: true
+        }
     }
 
     if (SENSOR) {
         SENSOR.unexport(); // Unexport GPIO to free resources
     }
+
+    return { message: `Nothing was spotting`, success: true }
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+    let response: ResponseData = { message: '', success: false }
     try {
         if (req.method === 'POST') {
             try {
                 const body = req.body as Config;
 
                 if (body?.enabled) {
-                    startSpotting();
+                    response = startSpotting();
                 } else {
-                    stopSpotting();
+                    response = stopSpotting();
                 }
 
                 fs.promises.writeFile(fullFilePath, JSON.stringify(body));
-                res.status(200).send({ message: 'success' });
+                res.status(200).send(response);
             } catch (error) {
                 console.log(error);
-                res.status(500).send({ message: `Failed to write to ${fullFilePath}` });
+                res.status(500).send({ message: `Failed to write to ${fullFilePath}`, success: false });
             }
         }
 
         if (req.method === 'GET') {
             const message = await fs.promises.readFile(fullFilePath, 'utf8');
-            res.status(200).send({ message });
+            res.status(200).send({ message, success: true });
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send({ message: 'Something went wrong' });
+        res.status(500).send({ message: 'Something went wrong', success: false });
     }
 }
